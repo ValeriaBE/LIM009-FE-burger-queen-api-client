23c99752827cd51d9333f00a85c16432c6c462e2
Valeria Berrocal <valeriaberrocal@MBPdeValeria2.wifi>
2019-08-16 15:59:25 -0500

    home test

--- burger-queen/src/__tests__/components/Home.test.js
+++ burger-queen/src/__tests__/components/Home.test.js
@@ -4,6 +4,15 @@ import Home from '../../components/Home/index';
 import { fireEvent, cleanup, act, waitForDomChange, waitForElement } from '@testing-library/react';
 import { renderWithRouter, history } from '../utils';
 afterEach(cleanup)
+const spy = jest.spyOn(Storage.prototype, 'setItem');
+localStorage.setItem = spy;
+localStorage.setItem('user', JSON.stringify({
+  "_id": "1",
+  "email": "amy@gmail.com",
+  "roles": {
+    "admin": true
+  }
+}));
 it('renders without crashing', async() => {
   const historyMock = { push: jest.fn() };
   const { getByTestId } = renderWithRouter(<Home history={historyMock}/>);
@@ -42,4 +51,17 @@ it('renders without crashing', async() => {
     fireEvent.click(getByTestId('COCINA'))
   })
   expect(historyMock.push.mock.calls[0]).toEqual([ ("/cocina")]);
+});
+
+
+
+it('renders without crashing', async() => {
+  const historyMock = { push: jest.fn() };
+  const { getByTestId } = renderWithRouter(<Home history={historyMock}/>);
+
+  await waitForElement(() => getByTestId('header'))
+  act(() => {
+    fireEvent.click(getByTestId('ADMIN'))
+  })
+  expect(historyMock.push.mock.calls[0]).toEqual([ ("/admin")]);
 });
